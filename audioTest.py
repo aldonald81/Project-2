58b37b723f0cf2220bb675a41b6b18bb95c855dc
Alexander <aldonald@davidson.edu>
2023-04-13 15:27:03 -0400

    Updates

--- audioTest.py
+++ audioTest.py
@@ -1,19 +1,61 @@
-import sounddevice as sd
-import soundfile as sf
 
-# Define the sampling frequency and duration of the recording
-sample_rate = 44100
-duration = 5  # seconds
 
-# Use sounddevice to record the audio from the headphone jack
-print("Recording started")
-recording = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
+import speech_recognition as sr
 
-# Wait for the recording to finish
-sd.wait()
+listening = True
+while listening:
+    with sr.Microphone() as source:
+        recognizer = sr.Recognizer()
+        recognizer.adjust_for_ambient_noise(source)
+        recognizer.dynamic_energy_threshold = 3000
+        try:
+            print("Listening... ")
+            audio = recognizer.listen(source, timeout = 5.0)
+            response = recognizer.recognize_google(audio)
+            print(response)
+            #return response
+        except sr.UnknownValueError:
+            print("Didn't recognize that Doc!")
 
-# Save the recording to a WAV file
-sf.write("recording.wav", recording, sample_rate)
 
-# Print a message to indicate that the recording has finished
-print("Recording finished")
+"""
+import pyaudio
+import wave
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK,
+                input_device_index=2) #change this to your USB mic index
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+"""
---
+++ speechTranscription.py
@@ -0,0 +1,49 @@
+import speech_recognition as sr
+
+import os
+import openai
+
+# Set the file path for the .wav file to transcribe
+wav_file_path = "audio.wav"
+
+
+# Initialize recognizer class                                       
+r = sr.Recognizer()
+# audio object                                                         
+audio = sr.AudioFile("recipe.wav")
+#read audio object and transcribe
+with audio as source:
+    audio = r.record(source)                  
+    result = r.recognize_google(audio)
+    
+print(result)
+
+
+openai.api_key = "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"
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=result,
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+print(response)
+
+print(response.choices[0].text)
+"""
+# Open the .wav file
+with sr.AudioFile(wav_file_path) as source:
+    # Read the audio data from the file
+    audio_data = r.record(source)
+
+    # Use the recognizer to transcribe the audio to text
+    text = r.recognize_google(audio_data)
+
+# Print the transcribed text
+print("Transcribed text:")
+print(text)
+"""
